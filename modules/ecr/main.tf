561646df6bec78350b3ab976e69b4d308fc8082c
Eyup Canbay <eyupcanbay40@gmail.com>
2026-07-08 15:31:54 +0300

    Change ECR repository image tag mutability from IMMUTABLE to MUTABLE

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 resource "aws_ecr_repository" "app_repo" {
     name = var.repository_name
-    image_tag_mutability = "IMMUTABLE"
+    image_tag_mutability = "MUTABLE"
 
     image_scanning_configuration {
         scan_on_push = true
